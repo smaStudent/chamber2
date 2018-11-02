2110cc19bd943b708f903990cfd8146b0660fa80
smaStudent <43564930+smaStudent@users.noreply.github.com>
2018-11-02 09:30:58 +0100

    Update main_function.py

--- main_function.py
+++ main_function.py
@@ -1,67 +1,143 @@
 from chamberClass import Chamber
+
 from dataStruct import dataStruct
+
 import time
+
 from functions import *
+
+import time
+
 import serial
 
 
+
+
+
 def main():
-    # module to check if we are starting properly
+
+    # variables
+
     tryCount = 0
+
     amountOfData = 0
-    areWeWorking = True
-    while tryCount < 100:
-        try:
-            chamber = Chamber()
-            areWeWorking = True
-            tryCount = 0
-        except:
-            areWeWorking = False
-            print("Try again, we couldnt connect with chamber")
 
-        tempData = []
-        humiData = []
-        ##############################################
+    tempData = []
 
-        # module to run repeadly over and over
+    humiData = []
 
-        while areWeWorking:
+    ##### Try to connect with chamber
+
+    try:
+
+        chamber = Chamber()
+
+        areWeWorking = True
+
+    except:
+
+        while tryCount < 100:
 
             try:
-                temptData = dataStruct(chamber.getTemp())
-                humitData = dataStruct(chamber.getHumi())
-                tempData.append(temptData)                  # collecting data and if we succeed, we adding this to our table
-                humiData.append(humitData)                  # collecting data and if we succeed, we adding this to our table
-            except:
-                print("We have a problem")
-                # switch on RED LED for exaple
-                break
 
-            if amountOfData == 100:                         # check if we already have 100 piece of data in each tables
-                #here we have to add this to the MySQL
-                try:
-                    saveSomeDataToMySQL( 'chamberTemp', tempData)
-                    saveSomeDataToMySQL(, humiData)
-                    saveObjectToFile("tempData.txt", tempData)
-                    saveObjectToFile("HumiData.txt", humiData)
-                    print("Adding corectly to MySQl database")
-                except mysql.err.Error:
-                    saveObjectToFile("tempData.txt", tempData)
-                    saveObjectToFile("HumiData.txt", humiData)
-                    print("Added data just to the files")
-                amountOfData = 0
-                tempData.clear()
-                humiData.clear()
-            else:
+                chamber = Chamber()
+
+                areWeWorking = True
+
+                tryCount = 0
+
+            except:
+
+                print("Try again later, this was: " + str(tryCount) +
+
+                      "try. CHECK CABLES AND WE WILL TRY AGAIN IN 10 seconds")
+
+                areWeWorking = False
+
+                tryCount += tryCount
+
+                time.sleep(10)
+
+    #### end of module which want to connect with chamber
+
+
+
+    ######## module which is responsible for collectind data, and store that in the MySQL
+
+
+
+    while areWeWorking:
+
+        try:
+
+            if chamber.update():
+
+                temptData = dataStruct(chamber.getTemp())
+
+                humitData = dataStruct(chamber.getHumi())
+
                 amountOfData += 1
 
 
-        # if we are here, we will give 15 second for user to solve the connection problem etc.
-        time.sleep(15)
-        # adding one to tryCount
-        tryCount += 1
-        ##############################################
+
+            if amountOfData == 100:
+
+                for i in tempData:
+
+                    saveSomeDataToMySQL('mysql01.saxon.beep.pl',
+
+                                        'sub_saxon',
+
+                                        'passwd',
+
+                                        'test_database',
+
+                                        'chamberTemp',
+
+                                        i)
+
+
+
+                for i in humiData:
+
+                    saveSomeDataToMySQL('mysql01.saxon.beep.pl',
+
+                                        'sub_saxon',
+
+                                        'passwd',
+
+                                        'test_database',
+
+                                        'chamberHumi',
+
+                                        i)
+
+
+
+                tempData = []
+
+                humiData = []
+
+                amountOfData = 0
+
+                # if we would like to, we can add data also to the file and have local history
+
+
+
+        except:
+
+            print("Unable to update chamber or there is a problem with saving it in the MySQL!")
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
+
     main()
