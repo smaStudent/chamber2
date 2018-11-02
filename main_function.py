9dbe4988b041dba228d7b410a31600b24f35635b
sma.student <sma.student@interia.pl>
2018-11-02 12:55:42 +0100

    2

--- main_function.py
+++ main_function.py
@@ -12,7 +12,6 @@ def main():
     amountOfData = 0
     tempData = []
     humiData = []
-    saveDataInIteration = False
     ##### Try to connect with chamber
     try:
         chamber = Chamber()
@@ -40,34 +39,35 @@ def main():
                 humiData = dataStruct(chamber.getHumi())
                 amountOfData += 1
 
-            if amountOfData == 5 and not saveDataInInteration:
+            if amountOfData == 5:
                 for i in tempData:
-                    saveSomeDataToMySQL('mysql01.saxon.beep.pl',
-                                        'sub_saxon',
-                                        'passwd',
-                                        'test_database',
-                                        'chamberTemp',
-                                        i)
+                    try:
+                        saveSomeDataToMySQL('mysql01.saxon.beep.pl',
+                                            'sub_saxon',
+                                            'passwd',
+                                            'test_database',
+                                            'chamberTemp',
+                                            i)
+                    except:
+                        i.saveToFile("tempData.txt")
 
                 for i in humiData:
-                    saveSomeDataToMySQL('mysql01.saxon.beep.pl',
-                                        'sub_saxon',
-                                        'passwd',
-                                        'test_database',
-                                        'chamberHumi',
-                                        i)
+                    try:
+                        saveSomeDataToMySQL('mysql01.saxon.beep.pl',
+                                            'sub_saxon',
+                                            'passwd',
+                                            'test_database',
+                                            'chamberHumi',
+                                            i)
+                    except:
+                        i.saveToFile("humiData.txt")
 
                 tempData = []
                 humiData = []
                 amountOfData = 0
-                saveDataInIteration = True
                 # if we would like to, we can add data also to the file and have local history
-
         except:
             print("Unable to update chamber or there is a problem with saving it in the MySQL!")
-
-
-
 
 
 if __name__ == '__main__':
--- testing.py
+++ testing.py
@@ -6,3 +6,14 @@ import time
 var = 1
 var += 1
 print(var)
+
+data1 = dataStruct.dataStruct(datetime.datetime.now(), 13.0, 0.0, 0.0, 100.0)
+time.sleep(1)
+data2 = dataStruct.dataStruct(datetime.datetime.now(), 16.0, 0.1, 0.0, 1010.0)
+
+functions.saveSomeDataToMySQL('mysql01.saxon.beep.pl',
+                              'sub_saxon',
+                              'passwd',
+                              'test_database',
+                              'chamberTemp',
+                              data1)
