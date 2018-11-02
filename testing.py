679b4c3974149ea0527e8691ad2bd7dfb9ff5d80
sma.student <sma.student@interia.pl>
2018-11-02 14:07:59 +0100

    1

--- testing.py
+++ testing.py
@@ -2,18 +2,17 @@ import dataStruct
 import functions
 import datetime
 import time
+import pymysql as mysql
 
 var = 1
 var += 1
 print(var)
 
-data1 = dataStruct.dataStruct(datetime.datetime.now(), 13.0, 0.0, 0.0, 100.0)
-time.sleep(1)
-data2 = dataStruct.dataStruct(datetime.datetime.now(), 16.0, 0.1, 0.0, 1010.0)
+dataTab = dataStruct.dataStruct(datetime.datetime.now(), 13.0, 0.0, 0.0, 100.0)
 
 functions.saveSomeDataToMySQL('mysql01.saxon.beep.pl',
                               'sub_saxon',
                               'passwd',
                               'test_database',
-                              'chamberTemp',
-                              data1)
+                              'chamberHumi',
+                              dataTab)
